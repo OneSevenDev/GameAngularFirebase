878295fc2b031c053151129ba5196c30f0242801
Manuel Guarniz <meguarnizc@indracompany.com>
2020-02-19 01:24:12 -0500

    configuracion para produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDYW65_eVvvhVOy1lWMdzfocMOVO-3Drrg',
+    authDomain: 'gameangularfirebase.firebaseapp.com',
+    databaseURL: 'https://gameangularfirebase.firebaseio.com',
+    projectId: 'gameangularfirebase',
+    storageBucket: '',
+    messagingSenderId: '693162419831',
+    appId: '1:693162419831:web:0712c1a0c44daf33'
+  },
+  maxGamer: 2,
+  timerResponse: 10 // en segundo
 };
