f32d019f0dee585bde2aa9f8d0f48cd636d04c3d
Manuel Guarniz <meguarnizc@indracompany.com>
2020-04-16 12:09:12 -0500

    correcciond e rutas apra pase a produccion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,7 +35,7 @@ const routes: Routes = [
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     FormsModule,
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, { useHash: true }),
   ],
   providers: [
     CoreFirebaseService,
